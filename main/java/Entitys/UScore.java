512ea2fe1029b2c9f77e81db48392825bd77fea9
kloping <3474006766@qq.com>
2021-11-26 13:19:01 +0800

    update 2021.11.26

--- main/java/Entitys/UScore.java
+++ main/java/Entitys/UScore.java
@@ -1,5 +1,6 @@
 package Entitys;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,11 +14,23 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UScore implements Serializable {
+    // private Number score=1000;
+    //    private Number times=0;
+    //    private Number fz=0;
+    //    private Number times_=0;
+    //    private Number days=0;
+    //    private Number score_=200;
+    //    private Number k=-1;
+    //    private Number day=-1;
+    //    private Number timesDay=-1;
+    //    private Number who;
     private Long score = Long.valueOf(1000);
     private Long times = 0l;
     private Long fz = 0l;
+    @JSONField(name = "times_")
     private Long sTimes = 0l;
     private Long days = 0L;
+    @JSONField(name = "score_")
     private Long sScore = 200L;
     private Long K = -1L;
     private Long day = -1L;
