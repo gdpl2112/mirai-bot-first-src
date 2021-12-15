ea388b7720fc65a1d698a1e967c668b4b3439a7c
kloping <3474006766@qq.com>
2021-12-15 16:34:37 +0800

    update 2021.12.15

--- main/java/temp/M2.java
+++ main/java/temp/M2.java
@@ -1,12 +1,7 @@
 package temp;
 
-import net.mamoe.mirai.contact.Group;
-
 public class M2 {
     public static void main(String[] args) {
 
-    }
-
-    public static void m1(Group group) {
     }
 }
