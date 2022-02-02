39d9889a14b0dd5cada2549d9b30a188d98c635a
HRS-Computer <3474006766@qq.com>
2022-02-02 12:19:09 +0800

    update

--- main/java/Project/services/DetailServices/ac/entity/Ghost703.java
+++ main/java/Project/services/DetailServices/ac/entity/Ghost703.java
@@ -31,7 +31,7 @@ public class Ghost703 extends GhostWithGroup {
         l = l > getHp() ? getHp() : l;
         if (rand.nextInt(2) == 0) {
             long v1 = percentTo(Math.toIntExact(l), 15);
-            sendMessage("受到反甲效果:\n" + GameDetailService.beaten(who.getId(), -1, v1), who.getId().longValue());
+            sendMessage("受到反甲效果:\n受到" + v1 + "点反弹伤害\n" + GameDetailService.beaten(who.getId(), -1, v1), who.getId().longValue());
         }
         return super.updateHp(l, who);
     }
