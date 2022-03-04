896477456d73994b9970df4a048ca9c8e565738b
HRS-Computer <3474006766@qq.com>
2022-03-04 15:02:50 +0800

    update

--- main/java/io/github/kloping/mirai0/Main/BotStarter.java
+++ main/java/io/github/kloping/mirai0/Main/BotStarter.java
@@ -56,7 +56,7 @@ public class BotStarter {
         setterStarterApplication(BotStarter.class);
         startRegisterListenerHost(args);
         startedAfter();
-        Temp.main(args);
+//        Temp.main(args);
         System.out.println("==============================" + qq.getQq() + ":启动完成=======================================");
         Resource.println("运行的线程=》" + Thread.activeCount());
     }
