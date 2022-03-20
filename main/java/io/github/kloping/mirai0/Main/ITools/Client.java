0c34b1177ae66293da773f7d8f8a29020c2f1485
HRS-Computer <3474006766@qq.com>
2022-03-20 19:47:41 +0800

    update

--- main/java/io/github/kloping/mirai0/Main/ITools/Client.java
+++ main/java/io/github/kloping/mirai0/Main/ITools/Client.java
@@ -7,6 +7,7 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.SneakyThrows;
 
 import java.nio.charset.StandardCharsets;
 
@@ -79,54 +80,62 @@ public class Client implements Runnable {
     private EventLoopGroup workerGroup = new NioEventLoopGroup();
     private Bootstrap b = new Bootstrap();
 
+    {
+        b.group(workerGroup);
+        b.channel(NioSocketChannel.class);
+        b.option(ChannelOption.SO_KEEPALIVE, true);
+        b.handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                                CHContext = ctx;
+                                ByteBuf buf = (ByteBuf) msg;
+                                String dataStr = buf.toString(StandardCharsets.UTF_8);
+                                Resource.bot.getGroup(gid).sendMessage("mc server: " + dataStr);
+                            }
+
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx) {
+                                CHContext = ctx;
+                            }
+
+                            @Override
+                            public void channelReadComplete(ChannelHandlerContext ctx) {
+
+                            }
+
+                            @Override
+                            public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+                                cause.printStackTrace();
+                                ctx.close();
+                                CHContext = null;
+                            }
+                        });
+                    }
+                }).option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator(65535));
+    }
+
     @Override
     public void run() {
         try {
-            b.group(workerGroup);
-            b.channel(NioSocketChannel.class);
-            b.option(ChannelOption.SO_KEEPALIVE, true);
-            b.handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) {
-                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
-
-                                @Override
-                                public void channelRead(ChannelHandlerContext ctx, Object msg) {
-                                    CHContext = ctx;
-                                    ByteBuf buf = (ByteBuf) msg;
-                                    String dataStr = buf.toString(StandardCharsets.UTF_8);
-                                    Resource.bot.getGroup(gid).sendMessage("mc server: " + dataStr);
-                                }
-
-                                @Override
-                                public void channelActive(ChannelHandlerContext ctx) {
-                                    CHContext = ctx;
-                                }
-
-                                @Override
-                                public void channelReadComplete(ChannelHandlerContext ctx) {
-
-                                }
-
-                                @Override
-                                public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-                                    cause.printStackTrace();
-                                    ctx.close();
-                                    CHContext = null;
-                                }
-                            });
-                        }
-                    }).option(ChannelOption.SO_KEEPALIVE, true)
-                    .option(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator(65535));
             f = b.connect(ip, port).sync();
+            System.out.println("connected");
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            try {
-                f.channel().closeFuture().sync();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
+    }
+
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                f.channel().closeFuture().sync();
+            }
+        });
     }
 }
