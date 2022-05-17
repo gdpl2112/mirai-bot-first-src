e244a87a659a83a52a1848701996c0d6ed29eb8e
HRS-Computer <3474006766@qq.com>
2022-05-17 18:03:29 +0800

    update

--- main/java/Project/detailPlugin/NetMain.java
+++ main/java/Project/detailPlugin/NetMain.java
@@ -6,5 +6,5 @@ package Project.detailPlugin;
 public class NetMain {
     //public static final String ROOT_PATH = BotStarter.test ? "http://localhost" : "http://kloping.life";
 
-    public static final String ROOT_PATH = "http://localhost";
+    public static final String ROOT_PATH = "http://kloping.life";
 }
