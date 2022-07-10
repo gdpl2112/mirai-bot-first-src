08289c6d1df9e797cb9ca9774e2af5fefcc1ef35
HRS-Computer <3474006766@qq.com>
2022-07-10 23:12:02 +0800

    update

--- main/java/Project/dataBases/SourceDataBase.java
+++ main/java/Project/dataBases/SourceDataBase.java
@@ -17,22 +17,23 @@ public class SourceDataBase {
     private static final Map<Integer, File> ID2FILE = new HashMap<>();
 
     public SourceDataBase() {
+        init();
     }
 
     private static synchronized void init() {
-        if (!ID2FILE.isEmpty())
-            return;
+        if (!ID2FILE.isEmpty()) return;
         for (File file : new File("images/game").listFiles()) {
             String fn = file.getName();
-            Integer id = Integer.valueOf( Tool.tool.findNumberFromString(fn));
+            Integer id = Integer.valueOf(Tool.tool.findNumberFromString(fn));
             ID2FILE.put(id, file);
         }
+        System.out.println(ID2FILE);
     }
 
     public static String getImgPathById(Integer id, Boolean k) {
         if (ID2FILE.isEmpty()) init();
         if (ID2FILE.containsKey(id))
-            return k ?  Tool.tool.pathToImg(ID2FILE.get(id.intValue()).getAbsolutePath()) :
+            return k ? Tool.tool.pathToImg(ID2FILE.get(id.intValue()).getAbsolutePath()) :
                     ID2FILE.get(id.intValue()).getAbsolutePath();
         else return null;
     }
