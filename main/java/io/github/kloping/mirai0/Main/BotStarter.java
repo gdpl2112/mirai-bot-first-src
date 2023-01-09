18e0f595e2341978bc9b40c152a85995dc248b41
HRS-Computer <3474006766@qq.com>
2023-01-09 22:11:41 +0800

    update

--- main/java/io/github/kloping/mirai0/Main/BotStarter.java
+++ main/java/io/github/kloping/mirai0/Main/BotStarter.java
@@ -5,26 +5,16 @@ import Project.aSpring.SpringStarter;
 import Project.listeners.NbListener;
 import io.github.kloping.MySpringTool.StarterApplication;
 import io.github.kloping.MySpringTool.annotations.CommentScan;
-import io.github.kloping.MySpringTool.interfaces.AutomaticWiringParams;
-import io.github.kloping.common.Public;
-import io.github.kloping.initialize.FileInitializeValue;
 import io.github.kloping.mirai0.Main.Handlers.LittleHandler;
 import io.github.kloping.mirai0.Main.Handlers.MyHandler;
 import io.github.kloping.mirai0.Main.Handlers.SaveHandler;
 import io.github.kloping.mirai0.Main.ITools.Client;
 import io.github.kloping.mirai0.unitls.Tools.Tool;
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
-import net.mamoe.mirai.Bot;
-import net.mamoe.mirai.console.MiraiConsole;
 import net.mamoe.mirai.console.terminal.MiraiConsoleImplementationTerminal;
 import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader;
 import net.mamoe.mirai.event.GlobalEventChannel;
-import net.mamoe.mirai.utils.BotConfiguration;
 
 import java.nio.file.Paths;
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
 
 import static io.github.kloping.mirai0.Main.Resource.*;
 
@@ -39,11 +29,8 @@ public class BotStarter {
 
     public static void main(String[] args) {
         long t = System.currentTimeMillis();
-        Public.EXECUTOR_SERVICE.submit(() -> {
-            MiraiConsoleImplementationTerminal terminal = new MiraiConsoleImplementationTerminal(Paths.get("./works", "/console1"));
-            MiraiConsoleTerminalLoader.INSTANCE.startAsDaemon(terminal);
-        });
-
+        MiraiConsoleImplementationTerminal terminal = new MiraiConsoleImplementationTerminal(Paths.get("./works", "/console1"));
+        MiraiConsoleTerminalLoader.INSTANCE.startAsDaemon(terminal);
         setterStarterApplication(BotStarter.class);
         Boolean t0 = StarterApplication.Setting.INSTANCE.getContextManager().getContextEntity(Boolean.class, "env.test");
         test = t0 == null ? false : t0;
@@ -64,11 +51,7 @@ public class BotStarter {
     private static void loadMc() {
         String ip = SpringBootResource.getEnvironment().getProperty("mc.ip");
         if (ip != null)
-            new Client(
-                    ip,
-                    SpringBootResource.getEnvironment().getProperty("mc.port"),
-                    SpringBootResource.getEnvironment().getProperty("mc.gid")
-            );
+            new Client(ip, SpringBootResource.getEnvironment().getProperty("mc.port"), SpringBootResource.getEnvironment().getProperty("mc.gid"));
     }
 
     private static void initBot() {
@@ -78,9 +61,7 @@ public class BotStarter {
     public static void startRegisterListenerHost(String[] args) {
         GlobalEventChannel.INSTANCE.registerListenerHost(new MyHandler());
         GlobalEventChannel.INSTANCE.registerListenerHost(LittleHandler.contextManager.getContextEntity(LittleHandler.class));
-        GlobalEventChannel.INSTANCE.registerListenerHost(
-                StarterApplication.Setting.INSTANCE.getContextManager().getContextEntity(NbListener.class)
-        );
+        GlobalEventChannel.INSTANCE.registerListenerHost(StarterApplication.Setting.INSTANCE.getContextManager().getContextEntity(NbListener.class));
         GlobalEventChannel.INSTANCE.registerListenerHost(new SaveHandler(args));
     }
 }
