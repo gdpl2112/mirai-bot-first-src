c1c09622eb9975618f404fbd4e901178d786fa9e
HRS-Computer <3474006766@qq.com>
2023-06-03 13:22:09 +0800

    update

--- main/java/Project/aSpring/mcs/controllers/UtilsController0.java
+++ main/java/Project/aSpring/mcs/controllers/UtilsController0.java
@@ -6,10 +6,14 @@ import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.contact.Friend;
 import net.mamoe.mirai.message.data.Image;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URL;
 
@@ -37,4 +41,19 @@ public class UtilsController0 {
         }
         return "http://inews.gtimg.com/newsapp_bt/0/15444429680/0";
     }
+
+    @PostMapping("/uploadImg")
+    public String uploadImg(@RequestParam("pwd") String pwd, @RequestParam("file") @Nullable MultipartFile imageFile) {
+        if (!pwd.equals(pwd1)) return "";
+        try {
+            Friend friend = BootstarpResource.BOT.getBot().getAsFriend();
+            Image image = Contact.uploadImage(friend, new ByteArrayInputStream(imageFile.getBytes()));
+            return Image.queryUrl(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "http://inews.gtimg.com/newsapp_bt/0/15444429680/0";
+    }
+
+
 }
