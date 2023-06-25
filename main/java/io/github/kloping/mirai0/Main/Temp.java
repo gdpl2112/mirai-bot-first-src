81776c37efdff55f39b267a2caf456f1465f6b10
HRS-Computer <3474006766@qq.com>
2023-06-25 22:35:56 +0800

    update

--- main/java/io/github/kloping/mirai0/Main/Temp.java
+++ main/java/io/github/kloping/mirai0/Main/Temp.java
@@ -7,7 +7,7 @@ import static Project.commons.uitls.FileDirUtils.copyEves;
  */
 public class Temp {
     public static void main(String[] args) throws Exception{
-        copyEves("D:\\Projects\\TeamProjects\\gdpl2112\\main-bot\\src\\main\\resource",
+        copyEves("D:\\Projects\\TeamProjects\\gdpl2112\\main-bot\\src\\main\\resources",
                 "D:\\Projects\\TeamProjects\\gdpl2112\\main-bot\\target\\classes", true);
         BotStarter.main(new String[]{});
     }
