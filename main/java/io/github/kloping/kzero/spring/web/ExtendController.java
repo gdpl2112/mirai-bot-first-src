a30d210095b4cf40e6151f98d7179c8aa560c83f
Kloping <3474006766@qq.com>
2023-11-01 15:06:17 +0800

    update

--- main/java/io/github/kloping/kzero/spring/web/ExtendController.java
+++ main/java/io/github/kloping/kzero/spring/web/ExtendController.java
@@ -90,7 +90,6 @@ public class ExtendController {
         return UrlUtils.getStringFromHttpUrl(webUrl + "/accept?id=" + id + "&pwd=" + webPwd);
     }
 
-
     public static final char[] ALL_CHARS = new char[]{
             '1', '2', '3', '4', '5', '6', '7', '8', '9',
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
