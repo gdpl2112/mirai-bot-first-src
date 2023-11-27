2643797015646209122d4c2d563ec6cedfba94c0
Kloping <3474006766@qq.com>
2023-11-27 13:06:11 +0800

    fix music share

--- main/java/io/github/kloping/kzero/mirai/exclusive/CustomizeController.java
+++ main/java/io/github/kloping/kzero/mirai/exclusive/CustomizeController.java
@@ -6,11 +6,16 @@ import io.github.kloping.judge.Judge;
 import io.github.kloping.kzero.main.api.MessageSerializer;
 import io.github.kloping.kzero.mirai.exclusive.script.BaseMessageScriptContext;
 import io.github.kloping.kzero.mirai.exclusive.script.BaseScriptUtils;
+import io.github.kloping.kzero.mirai.exclusive.script.ScriptContext;
+import io.github.kloping.url.UrlUtils;
+import net.mamoe.mirai.Bot;
+import net.mamoe.mirai.contact.Contact;
+import net.mamoe.mirai.contact.User;
 import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.SimpleListenerHost;
-import net.mamoe.mirai.event.events.MessageEvent;
-import net.mamoe.mirai.event.events.MessagePostSendEvent;
-import net.mamoe.mirai.event.events.MessagePreSendEvent;
+import net.mamoe.mirai.event.events.*;
+import net.mamoe.mirai.message.data.ForwardMessageBuilder;
+import net.mamoe.mirai.message.data.Image;
 import net.mamoe.mirai.message.data.Message;
 import net.mamoe.mirai.message.data.MessageChain;
 import org.jetbrains.annotations.NotNull;
@@ -18,6 +23,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 
 /**
@@ -56,9 +62,101 @@ public class CustomizeController extends SimpleListenerHost {
         });
     }
 
+    @EventHandler
+    public void onEvent(BotEvent event) {
+        if (event instanceof MessageEvent) return;
+        if (event instanceof MessagePreSendEvent) return;
+        if (event instanceof MessagePostSendEvent) return;
+        if (event instanceof BotOnlineEvent) return;
+        if (event instanceof BotOfflineEvent) return;
+        final String code = getScriptCode(event.getBot().getId());
+        if (code == null) return;
+        Public.EXECUTOR_SERVICE.submit(() -> {
+            try {
+                ScriptEngine javaScript = SCRIPT_ENGINE_MANAGER.getEngineByName("JavaScript");
+                javaScript.put("context", new BasebBotEventScriptContext(event, serializer));
+                javaScript.put("utils", new BaseScriptUtils(event.getBot().getId(), template, serializer));
+                javaScript.put("msg", event.toString());
+                javaScript.put("event", event);
+                javaScript.eval(code);
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
     private String getScriptCode(long id) {
         File file = new File("./scripts/b-" + id + ".js");
         file.getParentFile().mkdirs();
         return FileUtils.getStringFromFile(file.getAbsolutePath());
     }
+
+
+    public static class BasebBotEventScriptContext implements ScriptContext {
+        private BotEvent event;
+        private MessageSerializer<Message> serializer;
+
+        public BasebBotEventScriptContext(BotEvent event, MessageSerializer<Message> serializer) {
+            this.event = event;
+            this.serializer = serializer;
+        }
+
+        @Override
+        public MessageChain getRaw() {
+            return null;
+        }
+
+        @Override
+        public void send(String str) {
+
+        }
+
+        @Override
+        public void send(Message message) {
+
+        }
+
+        @Override
+        public Bot getBot() {
+            return event.getBot();
+        }
+
+        @Override
+        public User getSender() {
+            return null;
+        }
+
+        @Override
+        public Contact getSubject() {
+            return null;
+        }
+
+        @Override
+        public ForwardMessageBuilder forwardBuilder() {
+            return new ForwardMessageBuilder(event.getBot().getAsFriend());
+        }
+
+        @Override
+        public Message deSerialize(String msg) {
+            return serializer.deserialize(msg);
+        }
+
+        @Override
+        public Image uploadImage(String url) {
+            try {
+                byte[] bytes = UrlUtils.getBytesFromHttpUrl(url);
+                Image image = Contact.uploadImage(event.getBot().getAsFriend(), new ByteArrayInputStream(bytes));
+                return image;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        public String getType() {
+            return event.getClass().getSimpleName();
+        }
+    }
+
 }
