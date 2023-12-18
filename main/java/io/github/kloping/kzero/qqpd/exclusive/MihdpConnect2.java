35dcb4712bea363a48be322d5702a9218f6a7a16
Kloping <3474006766@qq.com>
2023-12-18 11:31:08 +0800

    ok

--- main/java/io/github/kloping/kzero/qqpd/exclusive/MihdpConnect2.java
+++ main/java/io/github/kloping/kzero/qqpd/exclusive/MihdpConnect2.java
@@ -84,7 +84,7 @@ public class MihdpConnect2 extends ListenerHost implements MihdpClient.MihdpClie
                 return new GeneralData.ResDataImage(image.getUrl(), "http");
             else return new GeneralData.ResDataImage(image.getBytes());
         } else {
-            String text = sendAble.toString();
+            String text = sendAble.toString().trim();
             if (text.length() > 1 && text.startsWith("/")) text = text.substring(1);
             return new GeneralData.ResDataText(text.trim());
         }
