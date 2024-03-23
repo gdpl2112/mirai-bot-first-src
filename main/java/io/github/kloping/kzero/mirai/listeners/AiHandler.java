6ca6802c63743851cdbab0f102f96ee54d8e3991
Kloping <3474006766@qq.com>
2024-03-24 00:07:53 +0800

    update

--- main/java/io/github/kloping/kzero/mirai/listeners/AiHandler.java
+++ main/java/io/github/kloping/kzero/mirai/listeners/AiHandler.java
@@ -10,10 +10,12 @@ import net.mamoe.mirai.message.data.MusicKind;
 import net.mamoe.mirai.message.data.MusicShare;
 import net.mamoe.mirai.message.data.PlainText;
 import net.mamoe.mirai.message.data.SingleMessage;
+import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -26,34 +28,6 @@ public class AiHandler implements ListenerHost {
     public static final AiHandler INSTANCE = new AiHandler();
 
     public static final RestTemplate TEMPLATE = new RestTemplate();
-
-    @EventHandler
-    public void onE1(GroupMessageEvent event) {
-//        try {
-//            for (SingleMessage singleMessage : event.getMessage()) {
-//                if (singleMessage instanceof Image) {
-//                    String url0 = Image.queryUrl((Image) singleMessage);
-//                    try {
-//                        if (url0 != null) {
-//                            String data = TEMPLATE.getForObject("http://luck.klizi.cn/api/jianhuang.php?url=" + url0, String.class);
-//                            JSONObject jo0 = JSON.parseObject(data);
-//                            if (!jo0.getString("tips").equals("正常")) {
-//                                MessageChainBuilder builder = new MessageChainBuilder();
-//                                builder.append(String.format("分数:%s\n可能不正常\n来源:%s(%s)",
-//                                                jo0.get("score"), event.getGroup().getName(), event.getGroup().getId()))
-//                                        .append(singleMessage);
-//                                event.getBot().getGroup(570700910L).sendMessage(builder.build());
-//                            }
-//                        }
-//                    } catch (RestClientException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//        } catch (Throwable e) {
-//            e.printStackTrace();
-//        }
-    }
 
     // id <type,data>
     public static final Map<Long, SongData> QID2DATA = new HashMap<>();
@@ -71,6 +45,9 @@ public class AiHandler implements ListenerHost {
         }, 1, 2, TimeUnit.HOURS);
     }
 
+    public static final String TYPE_KUGOU = "KG";
+    public static final String TYPE_QQ = "qq";
+
     @EventHandler
     public void pointOnly(GroupMessageEvent event) throws Exception {
         StringBuilder line = new StringBuilder();
@@ -82,24 +59,20 @@ public class AiHandler implements ListenerHost {
         String out = line.toString().trim();
         if (out.startsWith("酷狗点歌") && out.length() > 4) {
             String name = out.substring(4);
-            QID2DATA.put(event.getSender().getId(), new SongData("kg", name, event.getSender().getId(), System.currentTimeMillis()));
-            event.getSubject().sendMessage(
-                    TEMPLATE.getForObject("https://xiaoapi.cn/API/yy.php?type=kg&msg=" + name, String.class)
-                            + "\n使用'取消点歌'/'取消选择'来取消选择");
+            kugouVip(event, name, 1);
             return;
         } else if (out.startsWith("网易点歌") && out.length() > 4) {
             String name = out.substring(4);
             QID2DATA.put(event.getSender().getId(), new SongData("wy", name, event.getSender().getId(), System.currentTimeMillis()));
             event.getSubject().sendMessage(
-                    TEMPLATE.getForObject("https://xiaoapi.cn/API/yy.php?type=wy&msg=" + name, String.class)
-                            + "\n使用'取消点歌'/'取消选择'来取消选择");
+                    TEMPLATE.getForObject("https://xiaoapi.cn/API/yy.php?type=wy&msg=" + name, String.class) + "\n使用'取消点歌'/'取消选择'来取消选择");
             return;
         } else if (out.startsWith("点歌") && out.length() > 2) {
             String name = out.substring(2);
-            pVip(event, name, 1);
+            qqvip(event, name, 1);
         } else if (out.startsWith("QQ点歌") && out.length() > 4) {
             String name = out.substring(4);
-            pVip(event, name, 1);
+            qqvip(event, name, 1);
         } else if (out.startsWith("取消点歌")||out.startsWith("取消选择")) {
             QID2DATA.remove(event.getSender().getId());
         } else if (out.matches("[\\d]+")) {
@@ -108,21 +81,26 @@ public class AiHandler implements ListenerHost {
             if (e != null) {
                 MusicShare share = null;
                 String type = e.type;
-                if (type.equals("kg")) {
-                    String lines = TEMPLATE.getForObject(String.format("https://xiaoapi.cn/API/yy.php?type=%s&msg=%s&n=%s", e.type, e.data, n), String.class);
-                    String[] args = lines.split("\n");
-                    share = new MusicShare(MusicKind.KugouMusic, args[1].substring(3), args[2].substring(3), args[3].substring(5), args[0].substring(3), args[3].substring(5));
+                if (type.equals(TYPE_KUGOU)) {
+                    if (n == 0) {
+                        kugouVip(event, e.name, e.p + 1);
+                    } else {
+                        JSONObject d0 = (JSONObject) e.data;
+                        d0 = d0.getJSONObject("data").getJSONArray("name").getJSONObject(n - 1);
+                                                        //http://www.dreamling.top/API/kugou/android/music/api.php?keyword=%E7%A8%BB%E9%A6%99&pagenum=10&format=json&flag=format
+                        Document doc0 = getDocument("http://www.dreamling.top/API/kugou/web/music/api.php?&pagenum=9&format=json&flag=format&page=" + e.p + "&keyword=" + e.name + "&n=" + n);
+                        JSONObject data = JSON.parseObject(doc0.body().text());
+                        String url = data.getJSONObject("data").getString("url");
+                        share = new MusicShare(MusicKind.KugouMusic, d0.getString("SongName"), d0.getString("SingerName"), url, d0.getString("Image"), url);
+                    }
                 } else if (type.equals("wy")) {
                     String lines = TEMPLATE.getForObject(String.format("https://xiaoapi.cn/API/yy.php?type=%s&msg=%s&n=%s", e.type, e.data, n), String.class);
                     String[] args = lines.split("\n");
                     share = new MusicShare(MusicKind.NeteaseCloudMusic, args[1].substring(3), args[2].substring(3), args[3].substring(5), args[0].substring(3), args[3].substring(5));
                     event.getSubject().sendMessage(share);
-                } else {
+                } else if (type.equals(TYPE_QQ)) {
                     if (n == 0) {
-                        String[] args = type.split("\\|");
-                        Integer p = Integer.valueOf(args[1]);
-                        String name = args[2];
-                        pVip(event, name, p + 1);
+                        qqvip(event, e.name, e.p + 1);
                     } else {
                         JSONObject d0 = (JSONObject) e.data;
                         d0 = d0.getJSONArray("list").getJSONObject(n - 1);
@@ -137,27 +115,10 @@ public class AiHandler implements ListenerHost {
         }
     }
 
-    /**
-     * 获取重定向地址
-     *
-     * @param path
-     * @return
-     * @throws Exception
-     */
-    private static String getRedirectUrl(String path) throws Exception {
-        Document doc0 = Jsoup.connect(path).ignoreHttpErrors(true).ignoreContentType(true).header("Connection", "Keep-Alive")
-                .header("User-Agent", "Apache-HttpClient/4.5.14 (Java/17.0.8.1)").header("Accept-Encoding", "br,deflate,gzip,x-gzip").get();
-        return doc0.location();
-    }
-
-    private static void pVip(GroupMessageEvent event, String name, Integer p) throws Exception {
-        Document doc0 = Jsoup.connect("https://zj.v.api.aa1.cn/api/qqmusic/demo.php?type=1&n=9&p=" + p + "&q=" + name)
-                .ignoreHttpErrors(true).ignoreContentType(true)
-                .header("Connection", "Keep-Alive")
-                .header("User-Agent", "Apache-HttpClient/4.5.14 (Java/17.0.8.1)")
-                .header("Accept-Encoding", "br,deflate,gzip,x-gzip").get();
+    private static void qqvip(GroupMessageEvent event, String name, Integer p) throws Exception {
+        Document doc0 = getDocument("https://zj.v.api.aa1.cn/api/qqmusic/demo.php?type=1&n=9&p=" + p + "&q=" + name);
         JSONObject jo0 = JSON.parseObject(doc0.body().text());
-        QID2DATA.put(event.getSender().getId(), new SongData(String.format("data|%s|%s", p, name), jo0, event.getSender().getId(), System.currentTimeMillis()));
+        QID2DATA.put(event.getSender().getId(), new SongData(p, name, TYPE_QQ, jo0, event.getSender().getId(), System.currentTimeMillis()));
         if (jo0.getInteger("code") == 200) {
             StringBuilder sb = new StringBuilder();
             int i = 1;
@@ -173,6 +134,49 @@ public class AiHandler implements ListenerHost {
         return;
     }
 
+    private static void kugouVip(GroupMessageEvent event, String name, Integer p) throws Exception {
+        Document doc0 = getDocument("http://www.dreamling.top/API/kugou/web/music/api.php?&pagenum=9&format=json&flag=format&page=" + p + "&keyword=" + name);
+        JSONObject jo0 = JSON.parseObject(doc0.body().text());
+        QID2DATA.put(event.getSender().getId(), new SongData(p, name, TYPE_KUGOU, jo0, event.getSender().getId(), System.currentTimeMillis()));
+        if (jo0.getInteger("code") == 200) {
+            JSONObject data = jo0.getJSONObject("data");
+            StringBuilder sb = new StringBuilder();
+            sb.append("共搜索到'").append(data.getInteger("total")).append("'个结果").append(";当前第'").append(data.getInteger("page")).append("'页\n");
+            int i = 1;
+            for (Object o1 : data.getJSONArray("name")) {
+                JSONObject e0 = (JSONObject) o1;
+                sb.append(i++).append(".").append(e0.getString("Name")).append("\n");
+            }
+            sb.append("tips:选择'0'可翻向下一页\n使用'取消点歌'/'取消选择'来取消选择");
+            event.getSubject().sendMessage(sb.toString());
+        } else {
+            event.getSubject().sendMessage(jo0.getString("搜索异常啦o(╥﹏╥)o"));
+        }
+        return;
+    }
+
+    @NotNull
+    private static Document getDocument(String url) throws IOException {
+        Document doc0 = Jsoup.connect(url)
+                .ignoreHttpErrors(true).ignoreContentType(true)
+                .header("Connection", "Keep-Alive")
+                .header("User-Agent", "Apache-HttpClient/4.5.14 (Java/17.0.8.1)")
+                .header("Accept-Encoding", "br,deflate,gzip,x-gzip").get();
+        return doc0;
+    }
+
+    /**
+     * 获取重定向地址
+     *
+     * @param path
+     * @return
+     * @throws Exception
+     */
+    private static String getRedirectUrl(String path) throws Exception {
+        Document doc0 = getDocument(path);
+        return doc0.location();
+    }
+
     public static class SongData {
         public SongData(String type, Object data, Long qid, Long time) {
             this.type = type;
@@ -181,6 +185,17 @@ public class AiHandler implements ListenerHost {
             this.time = time;
         }
 
+        public SongData(Integer p, String name, String type, Object data, Long qid, Long time) {
+            this.p = p;
+            this.name = name;
+            this.type = type;
+            this.data = data;
+            this.qid = qid;
+            this.time = time;
+        }
+
+        private Integer p;
+        private String name;
         private String type;
         private Object data;
         private Long qid;
