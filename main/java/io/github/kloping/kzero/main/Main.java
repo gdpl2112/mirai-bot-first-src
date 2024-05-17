60bfbcd97653882749e8fedc9319b5bad33bd1d8
Kloping <3474006766@qq.com>
2024-05-17 09:48:33 +0800

    update

--- main/java/io/github/kloping/kzero/main/Main.java
+++ main/java/io/github/kloping/kzero/main/Main.java
@@ -4,6 +4,8 @@ import io.github.kloping.MySpringTool.annotations.CommentScan;
 import io.github.kloping.kzero.hwxb.WxHookStarter;
 import io.github.kloping.kzero.mihdp.MihdpClient;
 import io.github.kloping.kzero.mirai.MiraiStater;
+import io.github.kloping.kzero.qqpd.GuildStater;
+import io.github.kloping.qqbot.api.Intents;
 
 /**
  * @author github.kloping
@@ -14,11 +16,11 @@ public class Main {
         new DevPluginConfig().run();
 
         KlopZeroMainThreads threads = new KlopZeroMainThreads();
-//        try {
-//            KlopZeroMainThreads.EXECUTOR_SERVICE.submit(new MihdpClient());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        try {
+            KlopZeroMainThreads.EXECUTOR_SERVICE.submit(new MihdpClient());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 //        try {
 //            KlopZeroMainThreads.EXECUTOR_SERVICE.submit(new GsuidClient());
 //        } catch (URISyntaxException e) {
@@ -26,11 +28,11 @@ public class Main {
 //        }
 //        threads.add(new WxStarter());
 //        threads.add(new WxHookStarter());
-        threads.add(new MiraiStater());
+//        threads.add(new MiraiStater());
 //        threads.add(new GuildStater("102057448", "v0uQvq74AZtFGTCCWcDnEpsOLNoszA2H", Intents.PRIVATE_INTENTS.getCode()));
 //        threads.add(new GuildStater("102005968", "SHxLuZlWTtqElfokFx6pNYX1qH9dFXN2", Intents.PRIVATE_INTENTS.getCode()));
-//        threads.add(new GuildStater("102032364", "pzlH9hVZ7KmIHgOzzhFYZNpaQHgs5fEF", "Z2IK7fz4tTvAAvRi",
-//                Intents.PUBLIC_INTENTS.and(Intents.GROUP_INTENTS)));
+        threads.add(new GuildStater("102032364", "pzlH9hVZ7KmIHgOzzhFYZNpaQHgs5fEF", "Z2IK7fz4tTvAAvRi",
+                Intents.PUBLIC_INTENTS.and(Intents.GROUP_INTENTS)));
         threads.run();
     }
 }
