ea4fac8fd9f433b485f0a3dc78f60ce745850b4d
Kloping <3474006766@qq.com>
2024-08-01 15:57:38 +0800

    修复了在获取用户信息时可能出现的RequestException异常，并更新了测试方法和头衔获取逻辑。

--- main/java/io/github/kloping/kzero/main/DevMain.java
+++ main/java/io/github/kloping/kzero/main/DevMain.java
@@ -23,8 +23,8 @@ public class DevMain {
         threads.add(new MiraiStater());
         threads.add(new GuildStater("102032364", "pzlH9hVZ7KmIHgOzzhFYZNpaQHgs5fEF", "Z2IK7fz4tTvAAvRi",
                 Intents.PUBLIC_INTENTS.and(Intents.GROUP_INTENTS)));
-        threads.add(new GuildStater("102005968", "SHxLuZlWTtqElfokFx6pNYX1qH9dFXN2",
-                Intents.PRIVATE_INTENTS.getCode()));
+//        threads.add(new GuildStater("102005968", "SHxLuZlWTtqElfokFx6pNYX1qH9dFXN2",
+//                Intents.PRIVATE_INTENTS.getCode()));
 //        threads.add(new WxStarter());
 //        threads.add(new WxHookStarter());
         try {
