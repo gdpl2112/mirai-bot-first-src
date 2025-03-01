2821afe03f513d05c16fb62e0197fc314b2ed45b
Kloping <3474006766@qq.com>
2025-03-01 23:34:17 +0800

    update

--- main/java/io/github/kloping/kzero/main/DevMain.java
+++ main/java/io/github/kloping/kzero/main/DevMain.java
@@ -17,7 +17,7 @@ import java.net.URISyntaxException;
 @ComponentScan("io.github.kloping.kzero")
 public class DevMain {
     public static void main(String[] args) {
-        System.err.println("build time 2024/12/22");
+        System.err.println("build time 2025/3/1");
         new DevPluginConfig().run();
         KlopZeroMainThreads threads = new KlopZeroMainThreads();
         threads.add(new MiraiStater());
