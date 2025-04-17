d192603ea2283f0b1f376b88255be16c2fb2731d
Kloping <3474006766@qq.com>
2025-04-17 16:07:48 +0800

    refactor: 更新构建时间并注释掉 GuildStater 初始化代码 - 更新构建时间从 2025/3/1 到 2025/4/17 - 注释掉 GuildStater 初始化代码，可能为了暂时禁用某个特定的启动配置

--- main/java/io/github/kloping/kzero/main/DevMain.java
+++ main/java/io/github/kloping/kzero/main/DevMain.java
@@ -18,12 +18,12 @@ import java.net.URISyntaxException;
 @ComponentScan("io.github.kloping.kzero")
 public class DevMain {
     public static void main(String[] args) {
-        System.err.println("build time 2025/3/1");
+        System.err.println("build time 2025/4/17");
         new DevPluginConfig().run();
         KlopZeroMainThreads threads = new KlopZeroMainThreads();
         threads.add(new MiraiStater());
-        threads.add(new GuildStater("102032364", "pzlH9hVZ7KmIHgOzzhFYZNpaQHgs5fEF", "Z2IK7fz4tTvAAvRi",
-                Intents.PUBLIC_INTENTS.and(Intents.GROUP_INTENTS)));
+//        threads.add(new GuildStater("102032364", "pzlH9hVZ7KmIHgOzzhFYZNpaQHgs5fEF", "Z2IK7fz4tTvAAvRi",
+//                Intents.PUBLIC_INTENTS.and(Intents.GROUP_INTENTS)));
 //        threads.add(new GuildStater("102005968", "SHxLuZlWTtqElfokFx6pNYX1qH9dFXN2",
 //                Intents.PRIVATE_INTENTS.getCode()));
 //        threads.add(new WxHookStarter());
